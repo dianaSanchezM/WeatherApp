bb13be89a191ce4df677136019f27f001fb19756
dianaSanchezM <diana.sanchez-m@mail.escuelaing.edu.co>
2018-10-25 16:19:07 -0500

    first commit

---
+++ src/weatherapp/WeatherApp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package weatherapp;
+
+/**
+ *
+ * @author 2108310
+ */
+public class WeatherApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
